fc21f2622f9384cb69f547eae0faa419855402ea
viniosouza <vinicios2508@gmail.com>
2018-03-07 19:47:34 -0300

    chore: store in app || Provider installed || Switch added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import thunk from "redux-thunk";
 import Home from "./pages/homePage";
 import Navbar from "./components/navbar";
+import Dashboard from "./pages/dashboard";
 import "./App.css";
+import store from "./services/store";
+import { Provider } from "react-redux";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact component={Home} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
