7b6707a058084ec7f09be9365657382cbf14b6eb
viniosouza <vinicios2508@gmail.com>
2018-03-07 19:50:48 -0300

    refactor: AuthActions refactored

--- src/services/redux/actions/AuthActions.js
+++ src/services/redux/actions/AuthActions.js
@@ -1,19 +1,33 @@
 import * as Endpoint from "../../api/EndpointAPI";
-import { FCB_AUTH, FCB_AUTH_FAILED } from "../actions/actionTypes";
+import { FCB_AUTH, FCB_AUTH_FAILED, LOGOUT } from "../actions/actionTypes";
 import axios from "axios";
 
-const config = {};
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  }
+};
 
 export const loginFacebook = () => dispatch => {
   axios.post(`${Endpoint.api}/Usuario/`, config).then(res => {
+    localStorage.setItem("acess-token", res.data.token);
+    localStorage.setItem("picture", res.data.picture);
     dispatch({
       type: FCB_AUTH,
       payload: res.data
     }).catch(error => {
+      localStorage.removeItem("acess-token");
+      localStorage.removeItem("picture");
       dispatch({
         type: FCB_AUTH_FAILED,
         payload: error
       });
+      return Promise.reject(error);
     });
   });
 };
+
+export function logout() {
+  return { type: LOGOUT };
+}
