59140bbf79c1e11eb6f0b497356c89f3f6f870c4
viniosouza <vinicios2508@gmail.com>
2018-03-12 21:16:23 -0300

    chore: NotFound Route Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeContainer from "./containers/HomePageContainer";
 import Navbar from "./components/navbar";
 import Dashboard from "./pages/dashboard";
+import NotFound from "./components/notFound";
 import "./App.css";
 import store from "./services/store";
 import { Provider } from "react-redux";
@@ -16,7 +17,8 @@ class App extends Component {
             <Navbar />
             <Switch>
               <Route path="/" exact component={HomeContainer} />
-              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/dashboard"  component={Dashboard} />
+              <Route path="*" component={NotFound} />
             </Switch>
           </div>
         </Router>
