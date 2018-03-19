6d0fe24ae9800b3ff71af6a3096a689b1f673a60
viniosouza <vinicios2508@gmail.com>
2018-03-19 07:23:21 -0300

    chore:  New logo added

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,28 +1,38 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import "./styles/Dashboard.scss";
-import Cardiogram from "../../images/cardiogram.png";
 import Vini from "../../images/vinicios.jpg";
+import Calendar from "../../components/icons/calendar";
+import Mail from "../../components/icons/mail";
+import Placeholder from "../../components/icons/placeholder";
+import Heart from "../../components/icons/heart";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.FB = props.fb;
     this.state = {
-      name: "",
-      picture: "",
       redirect: false
     };
+    console.log(props.fb);
+  }
+
+  componentDidMount() {
+    // this.FB.api("/100024880491575", "GET", {}, function(response) {
+    //   // Insert your code here
+    //   console.log(response)
+    // });
   }
 
   render() {
-    if (!localStorage.getItem("accessToken") || this.state.redirect) {
-      return <Redirect to="/" />;
-    }
+    // if (!localStorage.getItem("accessToken") || this.state.redirect) {
+    //   return <Redirect to="/" />;
+    // }
 
     return (
       <div className="dashboard-container">
         <h1 className="dashboard-container-title">
-          Seja bem-vindo Vinicios {this.props.lastName}
+          Olá Vinicios ! {this.props.lastName}
         </h1>
         <div className="dashboard-container-align">
           <div className="dashboard-container-middle">
@@ -33,19 +43,25 @@ class Dashboard extends Component {
               <h1 className="dashboard-container-name">Vinicios Oliveira</h1>
               <span className="dashboard-container-info">
                 {" "}
-                <p>vinicios2508@gmail.com</p>
-                <p>São Paulo - SP</p>
-                <br />
-                <h4>Frequência cardiaca</h4>
+                <p>
+                  <Calendar />
+                  <b>Data de nascimento:</b> 25/08/1999 - 19 anos
+                </p>
+                <p>
+                  {" "}
+                  <Mail />vinicios2508@gmail.com
+                </p>
+                <p>
+                  {" "}
+                  <Placeholder />São Paulo - SP
+                </p>
               </span>
               <div>
-                {" "}
+                <h3 className="dashboard-container-icon-title">
+                  Frequência cardiaca
+                </h3>{" "}
                 <span className="dashboard-container-icon">
-                  <img
-                    src={Cardiogram}
-                    className="dashboard-container-icon-heart"
-                    alt=""
-                  />{" "}
+                  <Heart />
                   0 bpm{" "}
                 </span>
               </div>
